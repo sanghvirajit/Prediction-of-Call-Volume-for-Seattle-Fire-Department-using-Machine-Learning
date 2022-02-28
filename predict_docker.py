df16661c117de3ef31ab4c934d8bd7d376bb44d0
unknown <rajit.msanghvi@gmail.com>
2022-02-28 11:40:00 +0100

    initial commit

--- predict_docker.py
+++ predict_docker.py
@@ -3,7 +3,7 @@ import requests
 url = 'http://localhost:9696/predict'
 
 input_ = {
-    'datetime': '2022-01-01 15:00:00'
+    'date': '2022-01-01'
 }
 
 response = requests.post(url, json=input_).json()
